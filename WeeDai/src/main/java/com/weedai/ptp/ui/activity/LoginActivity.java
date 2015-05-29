04d3c2fc4c28fcb95bdcc8181d4cb3f6ae66870e
rain-e <314365215@qq.com>
2015-05-29 12:00:39 +0800

    1.登录记住用户

--- WeeDai/src/main/java/com/weedai/ptp/ui/activity/LoginActivity.java
+++ WeeDai/src/main/java/com/weedai/ptp/ui/activity/LoginActivity.java
@@ -4,17 +4,24 @@ package com.weedai.ptp.ui.activity;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.error.VolleyError;
+import com.joanzapata.android.BaseAdapterHelper;
+import com.joanzapata.android.QuickAdapter;
 import com.weedai.ptp.R;
 import com.weedai.ptp.app.ApiClient;
 import com.weedai.ptp.constant.Config;
@@ -29,7 +36,11 @@ import com.weedai.ptp.view.ValidateCodeView;
 import com.weedai.ptp.volley.ResponseListener;
 
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
 
@@ -47,6 +58,17 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     private TextView tvLoginForgetPassword;
 
     private ProgressDialog progressDialog;
+
+    private CheckBox cbRememberUser;
+    private ImageView imgSelect;
+    private Spinner spinner;
+    private QuickAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<String>();
+    private Set<String> hashSet = new HashSet<String>();
+
+    private final String PREFERENCE_NAME = "userinfo";
+    private final String REMEBER_USER = "remeber_user";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,24 +95,91 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
         etValicode = (EditText) findViewById(R.id.etValicode);
+        cbRememberUser = (CheckBox) findViewById(R.id.cbRememberUser);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         tvLoginRegister = (TextView) findViewById(R.id.tvLoginRegister);
         tvLoginForgetPassword = (TextView) findViewById(R.id.tvLoginForgetPassword);
+        imgSelect = (ImageView) findViewById(R.id.imgSelect);
 
         btnLogin.setOnClickListener(this);
         tvLoginRegister.setOnClickListener(this);
         tvLoginForgetPassword.setOnClickListener(this);
+        imgSelect.setOnClickListener(this);
 
         viewValicode = (SimpleValidateCodeView) findViewById(R.id.viewValicode);
 //        viewValicode.getValidataAndSetImage(new String[]{"4","7","0","2"});
         viewValicode.setOnClickListener(this);
 
         getImgcode();  //获取验证码
+
+        adapter = new QuickAdapter<String>(LoginActivity.this, R.layout.listitem_user_remeber) {
+            @Override
+            protected void convert(final BaseAdapterHelper helper, final String item) {
+
+                helper.setText(R.id.tvUsername, item);
+                helper.setOnClickListener(R.id.imgDetele, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (adapter.getItem(helper.getPosition()).equals(etUsername.getText().toString())) {
+                            etUsername.getText().clear();
+                        }
+
+                        adapter.remove(item);
+                        dataList.remove(item);
+
+                        hashSet.clear();
+                        for (String item : dataList) {
+                            hashSet.add(item);
+                        }
+                        SharedPreferences preferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putStringSet(REMEBER_USER, hashSet);
+                        editor.commit();
+                    }
+                });
+
+                helper.getView().setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println("setOnClickListener");
+                        etUsername.setText(adapter.getItem(helper.getPosition()));
+                        spinner.setSelection(helper.getPosition());
+//                        spinner.
+                    }
+                });
+            }
+        };
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                etUsername.setText(adapter.getItem(position));
+                System.out.println("setOnItemSelectedListener");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        SharedPreferences preferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+        hashSet = preferences.getStringSet(REMEBER_USER, new HashSet<String>());
+        for (String item : hashSet) {
+            System.out.println("init remeber user " + item);
+            dataList.add(item);
+        }
+        adapter.addAll(dataList);
+
     }
+
 
     private void login() {
 
-        String username = etUsername.getText().toString();
+        final String username = etUsername.getText().toString();
         String password = etPassword.getText().toString();
         String code = etValicode.getText().toString();
 
@@ -137,6 +226,10 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                     }
                     Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
                     return;
+                }
+
+                if (cbRememberUser.isChecked()) {  //记住用户
+                    saveRemeberUser(username);
                 }
 
                 if (HomeFragment.isLoginFromHome) {
@@ -197,6 +290,34 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     }
 
 
+    private void showRemeberUser() {
+
+        SharedPreferences preferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+        hashSet = preferences.getStringSet(REMEBER_USER, new HashSet<String>());
+        dataList.clear();
+        for (String item : hashSet) {
+            System.out.println("showRemeberUser remeber user " + item);
+            dataList.add(item);
+        }
+//        adapter.replaceAll(dataList);
+        spinner.performClick();
+    }
+
+    private void saveRemeberUser(String username) {
+
+        dataList.add(0, username);
+        hashSet.clear();
+        for (String item : dataList) {
+            System.out.println("saveRemeberUser remeber user " + item);
+            hashSet.add(item);
+        }
+        SharedPreferences preferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putStringSet(REMEBER_USER, hashSet);
+        editor.commit();
+    }
+
+
     @Override
     public void onClick(View v) {
 
@@ -204,6 +325,9 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             case R.id.btnLogin:
 //                UIHelper.showMain(LoginActivity.this);
                 login();
+                break;
+            case R.id.imgSelect:
+                showRemeberUser();
                 break;
             case R.id.tvLoginRegister:
                 UIHelper.showRegister(LoginActivity.this);
